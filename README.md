b010735d5dbb96ac644805353771475a43a896f2
Ray Lin <ray.ntpc@gmail.com>
2017-09-13 16:05:27 +0800

    Add gulp-cshtml-minifier project

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# gulp-cshtml-minifier
-Minify ASP.NET Razor CSHTML
+# gulp-cshtml-minifier [![NPM version][npm-image]][npm-url]
+Minify ASP.NET Razor Views (.cshtml files) & REPLACE HASH to Datetime string
+
+## Usage
+
+First, install `gulp-cshtml-minifier` as a development dependency:
+
+```shell
+npm install --save-dev gulp-cshtml-minifier
+```
+
+Then, add it to your `gulpfile.js`:
+
+### Simple
+```javascript
+var minifyCshtml = require('gulp-cshtml-minifier');
+
+gulp.task('minify-cshtml', function(){
+  gulp.src('./Views/Shared/_Layout_Template.cshtml')
+    .pipe(minifyCshtml())
+    .pipe(gulp.dest('./Views/Shared/'));
+});
+```
+### Advanced
+```javascript
+var minifyCshtml = require('gulp-cshtml-minifier');
+var rename = require("gulp-rename"); // install gulp-rename
+var header = require('gulp-header'); // install gulp-header
+
+gulp.task('minify-cshtml', function(){
+  gulp.src(['./Views/Shared/_Layout_Template.cshtml'])
+    .pipe(rename('_Layout.cshtml')) // File Rename
+    .pipe(minifyCshtml({
+      htmlComments: true,   // Remove HTML comments <!-- -->
+      jsComments: true,     // Remove JS comments <!-- -->
+      razorComments: true,  // Remove Razor comments @* *@
+      whitespace: true      // Remove white-space
+      replaceHash: /hash/ig // Replace 'hash' to datetime string
+    }))
+    .pipe(header('\ufeff')) // Fixed encode issue
+    .pipe(gulp.dest('./Views/Shared/'));
+});
+```
+
+[npm-url]: https://npmjs.org/package/
---
+++ index.js
@@ -0,0 +1,81 @@
+'use strict';
+var Transform = require('readable-stream/transform');
+var rs = require('replacestream');
+
+module.exports = function(options) {
+  return new Transform({
+    objectMode: true,
+    transform: function(file, enc, callback) {
+      if (file.isNull()) {
+        // 不做處理
+        return callback(null, file);
+      }
+
+      function doReplace() {
+        if (file.isStream()) {
+          // file.contents 是一個 Stream - https://nodejs.org/api/stream.html
+          file.contents = file.contents.pipe(rs());
+          return callback(null, file);
+        }
+
+        if (file.isBuffer()) {
+          // file.contents 是 Buffer - https://nodejs.org/api/buffer.html
+          var temp = String(file.contents);
+
+          // Ideas: https://github.com/deanhume/html-minifier
+          //        https://github.com/RehanSaeed/gulp-minify-cshtml
+
+          if ((options !== undefined) && options.htmlComments) {
+            // <!--    - Match the start of the comment.
+            // [\s\S]* - Match anything in between.
+            // ?       - Or nothing at all.
+            // -->     - Match the end of the comment.
+            // g       - Match globally.
+            temp = temp.replace(/<!--[\s\S]*?-->/g, '');
+          }
+
+          if ((options !== undefined) && options.jsComments) {
+            // <!--    - Match the start of the comment.
+            // [\s\S]* - Match anything in between.
+            // ?       - Or nothing at all.
+            // -->     - Match the end of the comment.
+            // g       - Match globally.
+            temp = temp.replace(/\/\*[\s\S]*?\*\//g, '');
+          }
+
+          if ((options === undefined) || options.razorComments) {
+            // @\*     - Match the start of the comment.
+            // [\s\S]* - Match anything in between.
+            // ?       - Or nothing at all.
+            // \*@     - Match the end of the comment.
+            // g       - Match globally.
+            temp = temp.replace(/@\*[\s\S]*?\*@/g, '');
+          }
+
+          if ((options === undefined) || options.whitespace) {
+            // >           - Match the end of a tag.
+            // [\s]*       - Match any white-space.
+            // \<          - Match the start of a tag.
+            // (?!(\/pre)) - Do not match /pre. This stops removing white space between pre tags.
+            // gi          - Match globally and case insensitive.
+            temp = temp.replace(/>[\s]*\<(?!(\/pre))/gi, '><');
+          }
+
+          if (options.replaceHash && options.replaceHash instanceof RegExp) {
+            temp = temp.replace(options.replaceHash, new Date().getTime());
+          }
+
+          temp = temp.replace(/\s*\n\s*/gi, '\n');
+
+          file.contents = new Buffer(temp);
+
+          return callback(null, file);
+        }
+
+        callback(null, file);
+      }
+
+      doReplace();
+    }
+  });
+};
